4779866daf62c30f80c567544b067bb9a17d186d
Álex Cortiñas <mail@alexcortinas.es>
2023-06-02 11:49:34 +0200

    Sorting the test files

--- test/00-main.js
+++ test/00-main.js
@@ -1,28 +1,15 @@
 import assert from "assert";
 import parse from "../src/index.js";
-import store from "../src/store.js";
-import stripBom from "strip-bom";
-import fs from "fs";
-
-function readFile(filePath) {
-  return stripBom(fs.readFileSync(filePath, "utf8"));
-}
-
-function p(filePath) {
-  return `test/files/${filePath}`;
-}
-
-function resetProducts() {
-  store.reset();
-}
+import { resetProducts, readFile, p } from "./utils.js";
 
 suite("Main");
 
+before(resetProducts);
 beforeEach(resetProducts);
 
 test("Testing standard run", () => {
-  const input = readFile(p("00-input.txt"));
-  const expected = JSON.parse(readFile(p("00-expected.json")));
+  const input = readFile(p("00/00-input.txt"));
+  const expected = JSON.parse(readFile(p("00/00-expected.json")));
 
   const result = parse(input);
 
@@ -30,17 +17,24 @@ test("Testing standard run", () => {
 });
 
 test("Wrong entity in WMS layer", () => {
-  const input = readFile(p("01-invalid-input.txt"));
+  const input = readFile(p("00/01-invalid-input.txt"));
   assert.throws(() => {
     parse(input);
   }, /ERROR: entity Str2eet required by layer cityLayer does not exists!!/);
 });
 
 test("Testing more complex run", () => {
-  const input = readFile(p("02-input.txt"));
-  const expected = JSON.parse(readFile(p("02-expected.json")));
+  const input = readFile(p("00/02-input.txt"));
+  const expected = JSON.parse(readFile(p("00/02-expected.json")));
 
   const result = parse(input);
 
   assert.deepEqual(result, expected);
+});
+
+test("Testing product with syntax error", () => {
+  const input = readFile(p("00/03-syntax-error-input.txt"));
+  assert.throws(() => {
+    parse(input);
+  });
 });
